973c1a36d2aa8226d09a2b1b1342dbe2b39981be
Aman Sharma <92920588+Amansharma95@users.noreply.github.com>
2022-06-27 22:06:10 +0530

    Add files via upload

---
+++ Reverse a string.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverseString(string str)
+{
+    int n=str.size();
+    for(int i=0;i<n/2;i++)
+    {
+        swap(str[i],str[n-i-1]);
+    }
+    cout<<str;
+}
+
+int main()
+{
+    string str="Nagarro";
+
+
+    reverseString(str);
+    return 0;
+}
